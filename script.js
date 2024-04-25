7698071dea853e9008d46431b2edcd6c6d8fe5ce
istampers <125205472+istampers@users.noreply.github.com>
2024-04-25 09:43:15 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// JavaScript for Advertisement Carousel Animation
+const slides = document.querySelectorAll('.advertisement .slide');
+let currentSlide = 0;
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    if (i === index) {
+      slide.classList.add('active');
+    } else {
+      slide.classList.remove('active');
+    }
+  });
+}
+
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+}
+
+// Auto play the carousel
+setInterval(nextSlide, 3000);
